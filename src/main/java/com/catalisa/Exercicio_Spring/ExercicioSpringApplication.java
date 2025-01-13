3c70d12e3793aa729a1dc02d06490cc1b9f37e36
joaocandidozup <joao.candido@zup.com>
2025-01-13 15:33:56 -0300

    feat: start Spring Boot project

---
+++ src/main/java/com/catalisa/Exercicio_Spring/ExercicioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.catalisa.Exercicio_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalisa/Exercicio_Spring/ExercicioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalisa.Exercicio_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
